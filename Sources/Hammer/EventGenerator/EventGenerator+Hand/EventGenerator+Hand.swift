2127e2fb4ba9ec5d85436227fb974e56ffb19312
Soojin Ro <soojin.ro@momenti.tv>
2021-12-04 14:57:44 -0500

    Fix FingerUp causing UITouch to end in start location

--- Sources/Hammer/EventGenerator/EventGenerator+Hand/EventGenerator+Hand.swift
+++ Sources/Hammer/EventGenerator/EventGenerator+Hand/EventGenerator+Hand.swift
@@ -497,6 +497,7 @@ extension EventGenerator {
                 throw HammerError.touchForFingerDoesNotExist(index: finger.fingerIndex)
             }
 
+            try self.activeTouches.update(finger: finger, forIdentifier: existingIdentifier)
             return existingIdentifier
         case .ended, .cancelled:
             guard let existingIdentifier = existingIdentifier else {
